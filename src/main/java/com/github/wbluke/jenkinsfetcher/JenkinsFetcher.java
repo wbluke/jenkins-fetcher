4e815c95512390c5df4b23a369a063db6753876b
wbluke <pwb0127@gmail.com>
2021-03-17 23:35:18 +0900

    library init

---
+++ src/main/java/com/github/wbluke/jenkinsfetcher/JenkinsFetcher.java
@@ -0,0 +1,9 @@
+package com.github.wbluke.jenkinsfetcher;
+
+public class JenkinsFetcher {
+
+    public boolean someLibraryMethod() {
+        return true;
+    }
+
+}
---
+++ src/test/java/com/github/wbluke/jenkinsfetcher/JenkinsFetcherTest.java
@@ -0,0 +1,15 @@
+package com.github.wbluke.jenkinsfetcher;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JenkinsFetcherTest {
+
+    @Test
+    void testSomeLibraryMethod() {
+        JenkinsFetcher classUnderTest = new JenkinsFetcher();
+        assertThat(classUnderTest.someLibraryMethod()).isTrue();
+    }
+
+}
